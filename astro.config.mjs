26e1e69025601031ccec13312439f873659de3c8
tomcomtang <tomcomtang@tencent.com>
2025-11-13 11:51:30 +0800

    Change output mode to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   // adapter: edgeoneAdapter(),
   integrations: [react(), vue(), svelte(), mdx()]
 });
